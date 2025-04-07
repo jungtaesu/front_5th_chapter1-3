cb82da4ee29069d55e5c33a2b53a76ed98383d4e
jungtaesu <xotndi0808@naver.com>
2025-04-08 00:38:38 +0900

    deepEquals 작업중2

--- src/@lib/equalities/deepEquals.ts
+++ src/@lib/equalities/deepEquals.ts
@@ -1,3 +1,54 @@
-export function deepEquals<T>(objA: T, objB: T): boolean {
+export function deepEquals(objA: unknown, objB: unknown): boolean {
+  if (objA === objB) {
+    return true;
+  }
+
+  if (objA === null || objB === null) {
+    return false;
+  }
+
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    for (let i = 0; i < objA.length; i++) {
+      if (Array.isArray(objA[i]) || Array.isArray(objB[i])) {
+        return deepEquals(objA[i], objB[i]);
+      } else {
+        if (objA[i] !== objB[i]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  if (typeof objA === "object" && typeof objB === "object") {
+    console.log("objA:", objA, "objB:", objB);
+    const a = objA as Record<string, unknown>;
+    const b = objB as Record<string, unknown>;
+
+    const keysA = Object.keys(a);
+    const keysB = Object.keys(b);
+
+    if (keysA.length !== keysB.length) return false;
+
+    console.log("keysA:", keysA, "keysB:", keysB);
+
+    for (const key of keysA) {
+      if (!keysB.includes(key)) {
+        return false;
+      }
+      if (typeof a[key] === "object" && typeof b[key] === "object") {
+        if (!deepEquals(a[key], b[key])) {
+          return false;
+        }
+        return true;
+        //여기가 문제래
+      }
+
+    }
+
+    return true;
+  }
+
+
   return objA === objB;
 }
--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -1,8 +1,35 @@
-export function shallowEquals<T>(objA: T, objB: T): boolean {
+export function shallowEquals<T extends Record<string, unknown>>(objA: T, objB: T): boolean {
+  if (objA === objB) {
+    console.log("shallowEquals: same reference");
+    return true;
+  }
 
-  console.log('shallowEquals', objA, objB);
-  if(objA === objB) {
-    console.log('shallowEquals: same reference');
+  if (
+    (typeof objA === "string" && typeof objB === "string") ||
+    (Array.isArray(objA) && Array.isArray(objB))
+  ) {
+    if (objA.length !== objB.length) return false;
+
+    for (let i = 0; i < objA.length; i++) {
+      if (objA[i] !== objB[i]) return false;
+    }
+    return true;
+  }
+
+  if (
+    objA !== null &&
+    objB !== null &&
+    typeof objA === "object" &&
+    typeof objB === "object"
+  ) {
+    const keysA = Object.keys(objA);
+    const keysB = Object.keys(objB);
+    if (keysA.length !== keysB.length) return false;
+    console.log("objA:", objA, objB);
+    for (const key of keysA) {
+      if ((objA)[key] !== (objB)[key]) return false;
+    }
+    return true;
   }
 
   return objA === objB;
