57b5d639dbdd8f2029b8a0b0cccc20a3ff5ceed1
jungtaesu <xotndi0808@naver.com>
2025-04-07 23:11:32 +0900

    shallowEquals 부터 살펴보자

--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -1,3 +1,9 @@
 export function shallowEquals<T>(objA: T, objB: T): boolean {
+
+  console.log('shallowEquals', objA, objB);
+  if(objA === objB) {
+    console.log('shallowEquals: same reference');
+  }
+
   return objA === objB;
 }
