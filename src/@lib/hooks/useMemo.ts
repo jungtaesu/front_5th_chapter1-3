6547a02e119e4d3b05dc30549ac62eebb8a12d4a
jungtaesu <xotndi0808@naver.com>
2025-04-08 23:25:51 +0900

    useCallback 구현완료

--- src/@lib/hooks/useMemo.ts
+++ src/@lib/hooks/useMemo.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { DependencyList } from "react";
 import { shallowEquals } from "../equalities"; // Updated to accept DependencyList
 import { useRef } from "./useRef";
@@ -13,7 +12,13 @@ export function useMemo<T>(
   const memoized = useRef<{ deps: DependencyList; value: T } | null>(null);
   //이전 의존성과 결과를 저장할 ref value가 결과
 
-  if (memoized.current && _equals(memoized.current.deps as unknown as Record<string, unknown>, _deps as unknown as Record<string, unknown>)) {
+  if (
+    memoized.current &&
+    _equals(
+      memoized.current.deps as unknown as Record<string, unknown>,
+      _deps as unknown as Record<string, unknown>,
+    )
+  ) {
     //의존성이 변경되지 않은 경우
     return memoized.current.value;
   }
