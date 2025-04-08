aa819885c83f9e6e2576d99a5c75d284ead44f46
jungtaesu <xotndi0808@naver.com>
2025-04-08 16:07:15 +0900

    deepEquals, memo, deepMemo 작업완료

--- src/@lib/equalities/deepEquals.ts
+++ src/@lib/equalities/deepEquals.ts
@@ -1,21 +1,22 @@
 export function deepEquals(objA: unknown, objB: unknown): boolean {
+
   if (objA === objB) {
     return true;
   }
 
+  // if(typeof objA !== typeof objB) {
+  //   return false;
+  // };
   if (objA === null || objB === null) {
     return false;
   }
 
   if (Array.isArray(objA) && Array.isArray(objB)) {
+    if (objA.length !== objB.length) return false;
+    
     for (let i = 0; i < objA.length; i++) {
-      if (Array.isArray(objA[i]) || Array.isArray(objB[i])) {
-        return deepEquals(objA[i], objB[i]);
-      } else {
-        if (objA[i] !== objB[i]) {
-          return false;
-        }
-      }
+      //재귀 부분이 제일 헷갈린다. 깊은 비교를 했을때 한번이라도 false가 나온다면 false, 모두 같아야 true여야하기에 for 문 밖에서 return true;
+      if (!deepEquals(objA[i], objB[i])) return false;
     }
     return true;
   }
@@ -33,22 +34,13 @@ export function deepEquals(objA: unknown, objB: unknown): boolean {
     console.log("keysA:", keysA, "keysB:", keysB);
 
     for (const key of keysA) {
-      if (!keysB.includes(key)) {
-        return false;
-      }
-      if (typeof a[key] === "object" && typeof b[key] === "object") {
-        if (!deepEquals(a[key], b[key])) {
-          return false;
-        }
-        return true;
-        //여기가 문제래
-      }
-
+      if (!keysB.includes(key)) return false;
+      if (!deepEquals(a[key], b[key])) return false;
     }
 
     return true;
   }
 
 
-  return objA === objB;
+  return false;
 }
--- src/@lib/hocs/deepMemo.ts
+++ src/@lib/hocs/deepMemo.ts
@@ -2,6 +2,8 @@ import { deepEquals } from "../equalities";
 import { ComponentType } from "react";
 import { memo } from "./memo.ts";
 
-export function deepMemo<P extends object>(Component: ComponentType<P>) {
+export function deepMemo<P extends Record<string, unknown>>(Component: ComponentType<P>) {
+
   return memo(Component, deepEquals);
+
 }
